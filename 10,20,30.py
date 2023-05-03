9313fd43d0f30168e27dd93e441526a1f1f9be44
Juan Aguirre <116675833+AnubisGP@users.noreply.github.com>
2023-05-03 14:15:23 -0500

    Create 10,20,30.py

---
+++ 10,20,30.py
@@ -0,0 +1,15 @@
+import random
+x=0
+y=0
+z=1
+for i in range(1,10,1):
+    lis=random.randint(1,50)
+    print(lis)
+    if lis > 10 and lis < 20:
+        z += 1 
+        y = y+lis
+    elif lis < 30:
+        x += 1
+promedio= x/(z-1)
+print ("El promedio es: ",y )
+print ("Los nnumeros superiores a 30 son: ",x)
